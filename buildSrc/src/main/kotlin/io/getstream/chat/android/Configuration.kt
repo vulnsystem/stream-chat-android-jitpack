e4a9ef497da4a435a0af15dfa23df4413240cdbf
Gitcoins <gitcoins@outlook.com>
2024-01-17 14:29:56 +0800

    Update Configuration.kt

--- buildSrc/src/main/kotlin/io/getstream/chat/android/Configuration.kt
+++ buildSrc/src/main/kotlin/io/getstream/chat/android/Configuration.kt
@@ -8,7 +8,7 @@ object Configuration {
     const val majorVersion = 6
     const val minorVersion = 0
     const val patchVersion = 8
-    const val myVersion = 15
+    const val myVersion = 16
     const val versionName = "$majorVersion.$minorVersion.$patchVersion.$myVersion"
     const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
     const val artifactGroup = "io.gitcoins"
